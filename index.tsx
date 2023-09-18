1865c999a9642766c3ffe0954a36c5ed1bf6cbd0
Troy Marshall <tmarsha1@nd.edu>
2023-09-17 21:52:11 -0400

    populate todo array

--- index.tsx
+++ index.tsx
@@ -6,23 +6,38 @@ const server = Bun.serve({
     fetch: handler,
 });
 
+type Todo = { id: number, text: string };
+const todos: Todo[] = [];
+
 console.log(`Listening on http://${server.hostname}:${server.port}`)
 
-function handler(request: Request): Response {
+async function handler(request: Request): Promise<Response> {
     const url = new URL(request.url);
 
-    if (url.pathname === "" || url.pathname === "/")
+    if (url.pathname === "" || url.pathname === "/" || url.pathname === "/index.html") {
         return new Response(Bun.file("index.html"));
+    }
 
-    if (url.pathname === "/todos" && request.method === "GET")
-    return new Response(renderToString(<TodoList todos={[]} />));
+    if (url.pathname === "/todos" && request.method === "GET") {
+        return new Response(renderToString(<TodoList todos={todos} />));
+    }
         
-    if (url.pathname === "/todos" && request.method === "POST")
-        return new Response(renderToString(<TodoList todos={[]} />));
+    if (url.pathname === "/todos" && request.method === "POST") {
+        const { todo } = await request.json();
+
+        if (!todo?.length) return new Response("Invalid input", {status: 400});
+
+        todos.push({
+            id: todos.length + 1,
+            text: todo,
+        });
+
+        return new Response(renderToString(<TodoList todos={todos} />));
+    }
 
     return new Response("Not Found", { status: 404 });
 }
 
-function TodoList(props: { todos: { id: number, text: string } [] }) {
-    return <ul>{ props.todos.length ? props.todos.map(todo => <li>{todo.text}</li>) : 'No items added'}</ul>
+function TodoList(props: { todos: Todo[] }) {
+    return <ul>{ props.todos.length ? props.todos.map(todo => <li key={todo.id}>{todo.text}</li>) : 'No items added'}</ul>
 }
